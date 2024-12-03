2cce20351fec641666c62993119d3b515d5adb81
Isabella <149326422+bellamtm@users.noreply.github.com>
2024-12-03 12:53:50 -0700

    error checking ActivitySlot.py

--- ActivitySlot.py
+++ ActivitySlot.py
@@ -3,8 +3,7 @@ The 'ActivitySlot' class serves as the base class for all scheduling slots.
 It is an abstract class meant to represent time slots in a schedule, including games and practices.
 Specific slot types (e.g., GameSlot, PracticeSlot) inherit from this class and add their unique attributes.
 """
-from abc import ABC, abstractmethod
-from Activity import Activity
+from abc import ABC
 
 class ActivitySlot(ABC):
     pass
